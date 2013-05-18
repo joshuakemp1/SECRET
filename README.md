c05c8d691ac161ad001d0fbeb195b0ec7a181f65
joshua kemp <joshuakemp85@gmail.com>
2013-05-17 22:49:10 -0400

    adding app

---
+++ README.md
@@ -0,0 +1,1 @@
+This is Josh Kemp's RoR developer site.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SECRET::Application.config.secret_token = 'f4aebf9f58dc6417d9022cd88999cae0936b30c26979d7e9687079e126887877435c09c3d3815a43629500614fa03fd33291b2ac286cc713b625a7a1d5350680'
